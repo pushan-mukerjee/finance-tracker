bf9fb2c92cd21340fe6ad244c6bfa8271bd78d79
pushan-mukerjee <pushan.mukerjee@gmail.com>
2022-01-18 01:27:20 +1100

    implement add friend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,20 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following user #{friend.full_name}"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
     friend = User.find(params[:id])
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
-    flash[:notice] = "Stopped following friend #{friend.full_name}"
+    flash[:notice] = "Stopped following user #{friend.full_name}"
     redirect_to my_friends_path
   end
 end
